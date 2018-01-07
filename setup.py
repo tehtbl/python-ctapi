2d2065135de1671111841d81c00a64ff554dddcb
tehtbl <3999809+tehtbl@users.noreply.github.com>
2018-01-07 15:04:54 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python-cointracking-api',
+    version='0.1.0-dev',
+    packages=find_packages('ctapi'),
+    install_requires=[
+        'requests==2.18.4',
+        'future==0.16.0',
+        'PyYAML==3.11',
+    ],
+
+    author='tbl42',
+    author_email='cyberworker@posteo.de',
+    url='https://github.com/tbl42/python-cointracking-api',
+    description='Python Interface for CoinTracking.info API',
+    long_description=open('README.md').read(),
+    keywords = 'cointracking info btc api',
+    license='MIT',
+
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Office/Business :: Financial',
+    ]
+
+)
